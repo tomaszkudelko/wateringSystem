7c3ae497aaa0dabe2ba9480cfe3fb4f682dd28fa
Tomek <tomasz.kudelko@wp.pl>
2022-05-14 00:16:35 +0200

    Added mail handling

--- wateringSystem.ino
+++ wateringSystem.ino
@@ -9,6 +9,8 @@
 #include <WebSerial.h>
 #include <Adafruit_ADS1X15.h>
 #include <Wire.h>
+#include <ESP_Mail_Client.h>
+#include <Arduino.h>
 
 
 
@@ -21,6 +23,12 @@ const int DHT_PIN = 14;
 const int SCL_PIN = 5;
 const int SDA_PIN = 4;
 const int MOISTURE_PIN = A0;
+
+/* The SMTP Session object used for Email sending */
+SMTPSession smtp;
+/* Callback function to get the Email sending status */
+void smtpCallback(SMTP_Status status);
+boolean mailSent = false;
 
 // Time variables
 WiFiUDP ntpUDP;
@@ -81,8 +89,14 @@ void loop() {
   measureSensorValues();
   sendTelemetry();
 
-  if (needsWatering()) {
+  if (needsWatering() && !lowWaterLevel()) {
     waterPlants();
+    mailSent = false;
+  }
+
+  if (lowWaterLevel() && mailSent == false) {
+    sendMail();
+    mailSent = true;
   }
   delay(500);
   if (false) {
@@ -195,8 +209,81 @@ RPC_Callback callbacks[callbacks_size] = {
 
 /* --------------- MISC --------------- */
 
+void sendMail() {
+  /* Set the callback function to get the sending results */
+  smtp.callback(smtpCallback);
+  /* Declare the session config data */
+  ESP_Mail_Session session;
+
+  /* Set the session config */
+  session.server.host_name = SMTP_HOST;
+  session.server.port = SMTP_PORT;
+  session.login.email = AUTHOR_EMAIL;
+  session.login.password = AUTHOR_PASSWORD;
+  session.login.user_domain = "";
+
+  /* Declare the message class */
+  SMTP_Message message;
+
+  /* Set the message headers */
+  message.sender.name = "Watering system";
+  message.sender.email = AUTHOR_EMAIL;
+  message.subject = "Low water level";
+  message.addRecipient("Servant", RECIPIENT_EMAIL);
+
+  //Send raw text message
+  String textMsg = "Please refill the water tank";
+  message.text.content = textMsg.c_str();
+  message.text.charSet = "us-ascii";
+  message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+  
+  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_low;
+  message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
+  smtp.setSystemTime(timeClient.getEpochTime());
+
+  /* Connect to server with the session config */
+  if (!smtp.connect(&session))
+    return;
+
+  /* Start sending Email and close the session */
+  if (!MailClient.sendMail(&smtp, &message)) {
+    WebSerial.println("Error sending Email, " + smtp.errorReason());
+  }
+}
+
+/* Callback function to get the Email sending status */
+void smtpCallback(SMTP_Status status) {
+  /* Print the current status */
+  Serial.println(status.info());
+
+  /* Print the sending result */
+  if (status.success()){
+    Serial.println("----------------");
+    ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
+    ESP_MAIL_PRINTF("Message sent failled: %d\n", status.failedCount());
+    Serial.println("----------------\n");
+    struct tm dt;
+
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++){
+      /* Get the result item */
+      SMTP_Result result = smtp.sendingResult.getItem(i);
+      time_t ts = (time_t)result.timestamp;
+      localtime_r(&ts, &dt);
+
+      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
+      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
+      ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+    }
+    Serial.println("----------------\n");
+  }
+}
+
 boolean lowBattery() {
   return batteryVoltage < 3.4;
+}
+
+boolean lowWaterLevel() {
+  return waterLevelMoisturePercent < 10;
 }
 
 void initWiFi() {
